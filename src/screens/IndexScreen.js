e656a51ebf8572f9cd78f77f59cdeb5629c480bc
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2020-07-01 16:24:19 -0300

    setup navigator

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const IndexScreen = () => {
+  // test
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  )
+};
+
+export default IndexScreen;
