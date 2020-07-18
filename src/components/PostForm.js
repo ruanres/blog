a89ab6627ab4b8a8880f2ac6d65dc6dc7c508368
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2020-07-18 14:00:54 -0300

    extract PostForm component

---
+++ src/components/PostForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {
+  View, Text, TextInput, StyleSheet, Button,
+} from 'react-native';
+
+const PostForm = ({ onSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const onPress = () => {
+    onSubmit(title, content);
+    setTitle('');
+    setContent('');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Title:</Text>
+      <TextInput style={styles.input} value={title} onChangeText={setTitle} />
+      <Text style={styles.label}>Content:</Text>
+      <TextInput style={styles.input} value={content} onChangeText={setContent} />
+      <Button title="Submit" onPress={onPress} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 5,
+    margin: 10,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+  },
+});
+
+export default PostForm;
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,51 +1,16 @@
-import React, { useState } from 'react';
-import {
-  View, Text, TextInput, StyleSheet, Button,
-} from 'react-native';
+import React from 'react';
 import { useBlogContext } from '../context/BlogContext';
+import PostForm from '../components/PostForm';
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
   const { addBlogPost } = useBlogContext();
 
-  const onAdd = () => {
+  const onSubmit = (title, content) => {
     addBlogPost({ title, content });
-    setTitle('');
-    setContent('');
     navigation.navigate('Index');
   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput style={styles.input} value={title} onChangeText={setTitle} />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput style={styles.input} value={content} onChangeText={setContent} />
-      <Button title="Add Blog Post" onPress={onAdd} />
-    </View>
-  );
+  return <PostForm onSubmit={onSubmit} />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    padding: 5,
-    margin: 10,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 15,
-    padding: 5,
-  },
-});
 
 export default CreateScreen;
